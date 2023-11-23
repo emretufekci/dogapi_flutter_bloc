13ea5b6a27d9289b006cbd9d544c3720fe85a36c
emretufekci <contact@emretufekci.com>
2023-11-24 00:56:06 +0300

    loading improvements for data

--- lib/data/dog_repository.dart
+++ lib/data/dog_repository.dart
@@ -6,44 +6,33 @@ import '../model/dog_model.dart';
 class DogRepository {
   Future<List<Dog>> getDogList() async {
     final response = await http.get(Uri.parse('https://dog.ceo/api/breeds/list/all'));
-    var dogList = <Dog>[];
 
-      if (response.statusCode == 200) {
-        final dog = dogFromJson(response.body);
+    if (response.statusCode == 200) {
+      final dog = dogFromJson(response.body);
+      final dogList = <Dog>[];
 
-        dogList = dog.message.entries.map((entry) {
-          final breed = entry.key;
-          final subBreeds = entry.value;
-          final status = dog.status;
-          return Dog(message: {breed: subBreeds}, status: status, image: '');
-        }).toList();
-      } else {
-        throw Exception('Failed to load dogs');
-      }
-
-      for (var dog in dogList) {
-        final breed = dog.message.keys.first;
-        final imageStatus = dog.status;
+      await Future.wait(dog.message.entries.map((entry) async {
+        final breed = entry.key;
+        final subBreeds = entry.value;
         final imageResponse = await http.get(Uri.parse('https://dog.ceo/api/breed/$breed/images/random'));
-
 
         if (imageResponse.statusCode == 200) {
           final imageResponseJson = jsonDecode(imageResponse.body);
           final image = imageResponseJson['message'];
-          dog.image = image;
-          dog.status = imageStatus;
+          dogList.add(Dog(message: {breed: subBreeds}, status: dog.status, image: image));
         } else if (imageResponse.statusCode == 404) {
-          dog.image = '';
-          dog.status = imageStatus;
-        }
-        else {
+          dogList.add(Dog(message: {breed: subBreeds}, status: dog.status, image: ''));
+        } else {
           throw Exception('Failed to load image');
         }
-      }
-
+      }));
 
       return dogList;
+    } else {
+      throw Exception('Failed to load dogs');
     }
+  }
+
 
   Future<Map<String, List<String>>> getDogImages(String breed, String subBreed) async {
     final response = await http.get(Uri.parse('https://dog.ceo/api/breed/$breed/images'));
